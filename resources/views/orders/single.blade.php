21c5f0fd7db259341324e3aa1f3046980fc302fc
Tariq Dilrosun <42801356+TariqD@users.noreply.github.com>
2020-05-15 12:26:23 +0200

    Github issue #2 (Totaalprijs)

--- resources/views/orders/single.blade.php
+++ resources/views/orders/single.blade.php
@@ -4,7 +4,7 @@
     <div class="columns">
         <div class="column">
             <h1 id="h1" class="title has-padding-top-15" style="text-align: center;">
-                Bestelling
+                Bestelling #{{ $order->id }}
             </h1>
         </div>
     </div>
@@ -14,7 +14,14 @@
                 <div class="card">
                     <div class="card-content">
                         <div class="content">
-                            Klant: {{ $order->user }}
+                            Klant: {{ $order->user->email }} <br/>
+                            @if($order->paid >= 200)
+                                Verzendkosten: &euro;0,00<br/>
+                                Totaal: &euro; {{ $order->paid }}
+                            @else
+                                Verzendkosten: &euro;12,50<br/>
+                                Totaal: &euro; {{ $order->paid + 12.50 }}
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -31,29 +38,29 @@
     </div>
     <div class="columns has-margin-top-50 is-centered">
         <div class="columns">
-            <div class="column">
-                <div class="card">
-                    <header class="card-header">
-                        <p class="card-header-title">
-                          {{--  {{ $product->name }} --}}
-                        </p>
-                    </header>
-                    <div class="card-content">
-                        <div class="content">
-                            Prijs: &euro; {{--{{ $product->price }} --}}
+            @forelse($order->products as $product)
+                <div class="column">
+                    <div class="card">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                  {{ $product->name }}
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content">
+                                Prijs: &euro; {{ $product->price }}
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-content">
-                        <div class="content">
-                            @if($order->paid > 200 + 0 )
-                            Totaal: &euro; {{ $order->paid }}
-                                @else
-                                Totaal: &euro; {{ $order->paid + 12.50 }}
-                            @endif
+                        <div class="card-content">
+                            <div class="content">
+
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @empty
+            @endforelse
+
         </div>
     </div>
 @endsection
